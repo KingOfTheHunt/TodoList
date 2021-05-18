a2c6fa0d600b5fdcfa76735c81452ab9800edfe5
Davi <davifrancisco214@outlook.com>
2021-05-17 23:31:42 -0300

    Criando o controlador e duas rotas

---
+++ TodoList.Api/Controllers/TodoItemsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TodoList.Api.Models.ViewModel.TodoItem;
+using TodoList.Api.Services;
+
+namespace TodoList.Api.Controllers
+{
+    [Route("api/tasks")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly TodoItemService _service;
+
+        public TodoItemsController(TodoItemService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public async Task<IActionResult> Create([FromBody]TodoItemViewModelInput todoItem)
+        {
+            await _service.Insert(todoItem);
+
+            return Ok(todoItem);
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public async Task<IActionResult> Get()
+        {
+            var tasks = await _service.GetAll();
+
+            return Ok(tasks);
+        }
+    }
+}
