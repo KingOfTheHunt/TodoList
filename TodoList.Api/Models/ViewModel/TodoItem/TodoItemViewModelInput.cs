5225f06f4391f34e37626e583e8529b7e7589be0
Davi <davifrancisco214@outlook.com>
2021-05-17 21:50:44 -0300

    Criando a ViewModel que representa a criacao de uma tarefa

---
+++ TodoList.Api/Models/ViewModel/TodoItem/TodoItemViewModelInput.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Api.Models.ViewModel.TodoItem
+{
+    public class TodoItemViewModelInput
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public bool IsCompleted { get; set; }
+    }
+}
