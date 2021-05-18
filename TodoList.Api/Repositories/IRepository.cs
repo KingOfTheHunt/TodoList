bc553856dc3997c1fadc36e6483b6d8ad90097ae
Davi <davifrancisco214@outlook.com>
2021-05-17 23:28:09 -0300

    Criando o repositorio do TodoItem

---
+++ TodoList.Api/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TodoList.Api.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<List<T>> FindAll();
+        Task<T> Find(int id);
+        Task Save(T t);
+        Task Update(T t);
+        Task Delete(int id);
+    }
+}
---
+++ TodoList.Api/Repositories/TodoItemRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoList.Api.Data;
+using TodoList.Api.Models;
+
+namespace TodoList.Api.Repositories
+{
+    public class TodoItemRepository : IRepository<TodoItem>
+    {
+        private readonly TodoListDbContext _context;
+
+        public TodoItemRepository(TodoListDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Delete(int id)
+        {
+            var task = await Find(id);
+            _context.Remove(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<TodoItem> Find(int id)
+        {
+            var task = await _context.Tasks.FirstOrDefaultAsync(x => x.Id == id);
+
+            return task;
+        }
+
+        public async Task<List<TodoItem>> FindAll()
+        {
+            var tasks = await _context.Tasks.ToListAsync();
+
+            return tasks;
+        }
+
+        public async Task Save(TodoItem t)
+        {
+            _context.Add(t);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(TodoItem t)
+        {
+            _context.Update(t);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
