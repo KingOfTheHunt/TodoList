8996a0249d968fb2624c8625de67f65624647641
Davi <davifrancisco214@outlook.com>
2021-05-17 23:29:34 -0300

    Criando a classe de servico do TodoItem

---
+++ TodoList.Api/Services/TodoItemService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoList.Api.Models;
+using TodoList.Api.Models.ViewModel.TodoItem;
+using TodoList.Api.Repositories;
+
+namespace TodoList.Api.Services
+{
+    public class TodoItemService
+    {
+        private readonly TodoItemRepository _repository;
+
+        public TodoItemService(TodoItemRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Insert(TodoItemViewModelInput item)
+        {
+            var todoItem = new TodoItem
+            {
+                Name = item.Name,
+                IsCompleted = item.IsCompleted
+            };
+
+            await _repository.Save(todoItem);
+        }
+
+        public async Task<List<TodoItem>> GetAll()
+        {
+            return await _repository.FindAll();
+        }
+    }
+}
