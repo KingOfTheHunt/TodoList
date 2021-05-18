b578a7082e73f128a2acb62c4a8ed83a3de2fdf9
Davi <davifrancisco214@outlook.com>
2021-05-17 22:27:54 -0300

    Configurando a conexao com o MySQL

--- TodoList.Api/Startup.cs
+++ TodoList.Api/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using TodoList.Api.Data;
 
 namespace TodoList.Api
 {
@@ -26,6 +21,10 @@ namespace TodoList.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            // Configurando a conexão com o banco de dados.
+            services.AddDbContext<TodoListDbContext>(options => 
+                options.UseMySql(Configuration.GetConnectionString("TodoListDbContext"), 
+                builder => builder.MigrationsAssembly("TodoList.Api")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
