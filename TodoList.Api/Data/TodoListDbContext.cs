01dea1fd76cb14ddcbd5eb3a48fd4b2db1244929
Davi <davifrancisco214@outlook.com>
2021-05-17 22:26:04 -0300

    Criando o DbContext

---
+++ TodoList.Api/Data/TodoListDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoList.Api.Models;
+
+namespace TodoList.Api.Data
+{
+    public class TodoListDbContext : DbContext
+    {
+        public TodoListDbContext(DbContextOptions<TodoListDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> Tasks { get; set; }
+    }
+}
