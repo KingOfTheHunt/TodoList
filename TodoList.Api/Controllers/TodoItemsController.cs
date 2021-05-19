6ae0dc1f49f75d859b56f3c36bca1f53e850f445
Davi <davifrancisco214@outlook.com>
2021-05-18 23:08:08 -0300

    Adicionado novas rotas

--- TodoList.Api/Controllers/TodoItemsController.cs
+++ TodoList.Api/Controllers/TodoItemsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using TodoList.Api.Models;
 using TodoList.Api.Models.ViewModel.TodoItem;
 using TodoList.Api.Services;
 
@@ -22,16 +23,46 @@ namespace TodoList.Api.Controllers
         {
             await _service.Insert(todoItem);
 
-            return Ok(todoItem);
+            return Created("", todoItem);
         }
 
         [HttpGet]
         [Route("all")]
         public async Task<IActionResult> Get()
         {
-            var tasks = await _service.GetAll();
+            var tasks = await _service.GetTasks();
 
             return Ok(tasks);
+        }
+
+        [HttpGet]
+        [Route("find/{id}")]
+        public async Task<IActionResult> Get(int? id)
+        {
+            var task = await _service.GetTask(id);
+
+            if (task == null)
+            {
+                return NotFound("A tarefa não foi encontrada!");
+            }
+
+            return Ok(task);
+        }
+
+        [HttpPut]
+        [Route("update/{id}")]
+        public async Task<IActionResult> Update(int? id, [FromBody]TodoItem todoItem)
+        {
+            try
+            {
+                await _service.UpdateTask(todoItem);
+
+                return Ok(todoItem);
+            }
+            catch (System.Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
     }
 }
