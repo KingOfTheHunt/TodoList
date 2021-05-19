8e083e3c72065ce9626c493342995b16de7d8eb9
Davi <davifrancisco214@outlook.com>
2021-05-18 23:38:51 -0300

    Criando excecoes personalizadas

---
+++ TodoList.Api/Exceptions/DbUpdateException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoList.Api.Exceptions
+{
+    public class DbUpdateException : ApplicationException
+    {
+        public DbUpdateException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ TodoList.Api/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoList.Api.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message)
+            : base(message)
+        {
+        }
+    }
+}
